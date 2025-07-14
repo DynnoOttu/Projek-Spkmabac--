fad5d25e07ad7b23716313d80f28d7543f2be390
Dynno Yohanis Ottu <dynnoottu968@gmail.com>
2025-07-14 19:09:01 +0700

    perbaiki koneksi

--- includes/konek.php
+++ includes/konek.php
@@ -1,10 +1,7 @@
-<?php 
-$koneksi = mysqli_connect("localhost","root","","db_mabac");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "db_mabac");
 
-?>
- 
+// Cek koneksi
+if (!$koneksi) {
+	die("Koneksi database gagal: " . mysqli_connect_error());
+}
